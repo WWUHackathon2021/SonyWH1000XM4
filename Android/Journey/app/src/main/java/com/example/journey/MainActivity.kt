831fd69965174cefdb10aab4e0d71c717a74df8b
gunohdunno <kgjung253@gmail.com>
2021-04-10 16:09:25 -0500

    basic google maps (map not showing yet)

--- Android/Journey/app/src/main/java/com/example/journey/MainActivity.kt
+++ Android/Journey/app/src/main/java/com/example/journey/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.journey
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.example.journey.UI.MapsFragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         button.setOnClickListener {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
         }
     }
-
 }
