f4bf902deec5185691d6bd8179f54e735277f52d
mymddoan <mymddoan@gmail.com>
2021-04-10 17:20:19 -0700

    code fuck

--- Android/Journey/app/src/main/java/com/example/journey/MainActivity.kt
+++ Android/Journey/app/src/main/java/com/example/journey/MainActivity.kt
@@ -8,10 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import com.example.journey.UI.MapsFragment
 import com.example.journey.UI.ProfileFragment
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapFragment
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,25 +18,39 @@ import com.fondesa.kpermissions.anyPermanentlyDenied
 import com.fondesa.kpermissions.anyShouldShowRationale
 import com.fondesa.kpermissions.extension.permissionsBuilder
 import com.fondesa.kpermissions.request.PermissionRequest
+import com.google.android.gms.maps.*
 
 
-class MainActivity : AppCompatActivity(), PermissionRequest.Listener {
+class MainActivity : AppCompatActivity(), PermissionRequest.Listener, OnMapReadyCallback  {
     private var profilepage = false
     private val request by lazy {
         permissionsBuilder(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION).build()
     }
+
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
         request.addListener(this)
         request.send()
 
+
+        setContentView(R.layout.activity_main)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+
+
+        /*
         button.setOnClickListener {
 
             if (!profilepage){
                 val fragment = ProfileFragment.newInstance("","")
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.main, fragment)
+                    .replace(R.id.map, fragment)
                     .commitNow()
 
             }
@@ -53,6 +63,7 @@ class MainActivity : AppCompatActivity(), PermissionRequest.Listener {
 
             profilepage = !profilepage
         }
+         */
     }
 
     fun showGrantedToast() {
@@ -62,5 +73,15 @@ class MainActivity : AppCompatActivity(), PermissionRequest.Listener {
         when {
             result.allGranted() -> showGrantedToast()
         }
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
