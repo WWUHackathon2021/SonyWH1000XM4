c75157fa0e5c7fc808704b2b536bb14de2fa1554
adr-23 <adriangabales1@gmail.com>
2021-04-11 02:12:03 -0700

    login connected to server

--- Android/Journey/app/src/main/java/com/example/journey/LoginActivity.kt
+++ Android/Journey/app/src/main/java/com/example/journey/LoginActivity.kt
@@ -5,13 +5,19 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.ServerError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.gms.auth.api.signin.*
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.profile_ui.*
 
 
-class LoginActivity : AppCompatActivity() {
+const val LOGIN ="http://ec2-54-190-234-6.us-west-2.compute.amazonaws.com:5000/login"
+const val PING ="http://ec2-54-190-234-6.us-west-2.compute.amazonaws.com:5000/ping"
+class LoginActivity : AppCompatActivity(){
 
     lateinit var mGoogleSignInClient: GoogleSignInClient
     private val RC_SIGN_IN = 9001
@@ -63,20 +69,62 @@ class LoginActivity : AppCompatActivity() {
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(ApiException::class.java)
-            val Intent = Intent(this, MainActivity::class.java)
-            startActivity(Intent)
             val idToken = account!!.idToken
-            Toast.makeText(this, "id_token" + idToken, Toast.LENGTH_LONG).show()
+            sendRequest(idToken!!)
+            signInTest()
 
 
-            // TODO(developer): send ID Token to server and validate
 
         } catch (e: ApiException) {
-
             Log.v("SignInActivity", "handleSignInResult:error", e)
+
 
         }
     }
+
+    fun sendRequest(token: String){
+        try {
+            val queue = Volley.newRequestQueue(this)
+
+
+
+            val stringRequest = object: StringRequest(
+                    Request.Method.POST,
+                    LOGIN,
+                    { response ->
+                        // Display the first 500 characters of the response string.
+                        var msg = token
+                        Toast.makeText(this, "msg: "+msg, Toast.LENGTH_LONG).show()
+
+                    },
+                    { error ->
+                        var msg = "That didn't work!"
+                    })
+                    {
+                        override fun getBodyContentType(): String {
+                            return "application/x-www-form-urlencoded"
+                        }
+
+                        override fun getParams(): MutableMap<String, String> {
+                            var params = mutableMapOf<String, String>()
+                            params.put("token", token)
+                            return params
+                        }
+                    }
+
+            queue.add(stringRequest)
+        }
+
+        catch(e:ServerError){
+            Toast.makeText(this, ""+e, Toast.LENGTH_LONG).show()
+        }
+
+
+
+
+    }
+
+
 
 
 }
