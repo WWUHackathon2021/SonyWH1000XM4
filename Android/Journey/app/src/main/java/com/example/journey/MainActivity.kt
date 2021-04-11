4d42ee68f8e44a845803e93871ff0f898b669412
mymddoan <mymddoan@gmail.com>
2021-04-10 20:12:09 -0700

    map clickable

--- Android/Journey/app/src/main/java/com/example/journey/MainActivity.kt
+++ Android/Journey/app/src/main/java/com/example/journey/MainActivity.kt
@@ -15,10 +15,11 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.PointOfInterest
 
 
-class MainActivity : AppCompatActivity(), PermissionRequest.Listener, OnMapReadyCallback  {
+class MainActivity : AppCompatActivity(), PermissionRequest.Listener, OnMapReadyCallback, GoogleMap.OnPoiClickListener  {
     private var profilepage = false
     private val request by lazy {
         permissionsBuilder(
@@ -85,32 +86,35 @@ class MainActivity : AppCompatActivity(), PermissionRequest.Listener, OnMapReady
             mMap.setMyLocationEnabled(true);
             Toast.makeText(this, "SETLOCATIONTRUE", Toast.LENGTH_LONG).show()
         } else {
-            Toast.makeText(this, "You have to accept to enjoy all app's services!", Toast.LENGTH_LONG).show();
+            Toast.makeText(
+                this,
+                "You have to accept to enjoy all app's services!",
+                Toast.LENGTH_LONG
+            ).show();
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
                 mMap.setMyLocationEnabled(true);
             }
         }
 
-        /*
         if (mMap != null) {
             mMap.setOnMyLocationChangeListener { arg0 ->
-                mMap.addMarker(
-                    MarkerOptions().position(
-                        LatLng(
-                            arg0.latitude,
-                            arg0.longitude
-                        )
-                    ).title("It's Me!")
-                )
-            }
-            */
+                val currLocation = LatLng(arg0.latitude, arg0.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currLocation, 15.toFloat()))
 
-        /*
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-         */
+            }
+        }
+
+        googleMap.setOnPoiClickListener(this)
+
+
+    }
+
+    override fun onPoiClick(poi: PointOfInterest) {
+        Toast.makeText(this, """Clicked: ${poi.name}
+            Place ID:${poi.placeId}
+            Latitude:${poi.latLng.latitude} Longitude:${poi.latLng.longitude}""",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
